a10f86cceabfd9db95f812646441794f13fefdb4
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-14 13:49:46 +0700

    Function denga nama parameter tampildata
    di dalam function data dengan isi if else  dan for looping

---
+++ Tugas6.js
@@ -0,0 +1,12 @@
+function tampilkan_data() {
+    for (var i = 0; i <= 20; i++) {
+        console.log(i);
+    }        
+    console.log("");
+        if (20 % 2 == 0) {
+            console.log("Genap");
+        } else {
+            console.log(i);
+        }    
+}
+tampilkan_data();
